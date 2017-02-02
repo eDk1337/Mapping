3c5acb15fcaf75b6f8094c34696cb9de19376887
2ramae45 <edvinas211@gmail.com>
2017-02-02 12:54:34 +0000

    added a text box for longitude and atidute aswell as button.

--- app/src/main/java/com/example/a2ramae45/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2ramae45/mapping/HelloMap.java
@@ -5,8 +5,13 @@ import android.preference.PreferenceManager;
 import org.osmdroid.views.MapView;
 import org.osmdroid.util.GeoPoint;
 import org.osmdroid.config.Configuration;
+import android.widget.Button;
+import android.view.View.OnClickListener;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.EditText;
 
-public class HelloMap extends Activity
+public class HelloMap extends Activity implements View.OnClickListener
 {
 
     MapView mv;
@@ -16,6 +21,9 @@ public class HelloMap extends Activity
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button submitButton = (Button)findViewById(R.id.submitButton);
+        submitButton.setOnClickListener(this);
 
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
@@ -27,5 +35,15 @@ public class HelloMap extends Activity
         mv.setBuiltInZoomControls(true);
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
+    }
+
+    @Override
+    public void onClick(View view) {
+        EditText et = (EditText)findViewById(R.id.latitudeEditText);
+        double latitude = Integer.parseInt(et.getText().toString());
+
+        double latitude = 0;
+        double longitude = 0;
+        mv.getController().setCenter(new GeoPoint(latitude,longitude));
     }
 }
