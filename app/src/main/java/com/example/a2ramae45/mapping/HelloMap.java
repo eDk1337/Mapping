859f7bf6b96b52619b170a581493acbc1d28362c
edk1337 <edvinas211@gmail.com>
2017-02-09 11:36:07 +0000

    Added Menu, Select between Cycle and Normal map

--- app/src/main/java/com/example/a2ramae45/mapping/HelloMap.java
+++ app/src/main/java/com/example/a2ramae45/mapping/HelloMap.java
@@ -1,17 +1,18 @@
 package com.example.a2ramae45.mapping;
-import android.app.Activity;
-import android.os.Bundle;
-import android.preference.PreferenceManager;
-import org.osmdroid.views.MapView;
-import org.osmdroid.util.GeoPoint;
-import org.osmdroid.config.Configuration;
-import android.widget.Button;
-import android.view.View.OnClickListener;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.EditText;
 
-public class HelloMap extends Activity implements View.OnClickListener
+        import android.app.Activity;
+        import android.os.Bundle;
+        import android.preference.PreferenceManager;
+        import org.osmdroid.views.MapView;
+        import org.osmdroid.util.GeoPoint;
+        import org.osmdroid.config.Configuration;
+        import android.view.Menu;
+        import android.view.MenuInflater;
+        import android.view.MenuItem;
+        import android.content.Intent;
+        import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+
+public class HelloMap extends Activity
 {
 
     MapView mv;
@@ -21,9 +22,6 @@ public class HelloMap extends Activity implements View.OnClickListener
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        Button submitButton = (Button)findViewById(R.id.submitButton);
-        submitButton.setOnClickListener(this);
 
         // this line tells OpenStreetMap about our app.
         // If you miss this out, you might get banned from OSM servers
@@ -36,14 +34,46 @@ public class HelloMap extends Activity implements View.OnClickListener
         mv.getController().setZoom(14);
         mv.getController().setCenter(new GeoPoint(40.1,22.5));
     }
-
-    @Override
-    public void onClick(View view) {
-        EditText et = (EditText)findViewById(R.id.latitudeEditText);
-        double latitude = Integer.parseInt(et.getText().toString());
-
-        double latitude = 0;
-        double longitude = 0;
-        mv.getController().setCenter(new GeoPoint(latitude,longitude));
+    public boolean onCreateOptionsMenu(Menu menu)
+    {
+        MenuInflater inflater=getMenuInflater();
+        inflater.inflate(R.menu.menu_hello_map, menu);
+        return true;
     }
+
+
+    public boolean onOptionsItemSelected(MenuItem item)
+    {
+        if(item.getItemId() == R.id.choosemap)
+        {
+            // react to the menu item being selected...
+            Intent intent = new Intent(this,MapChooseActivity.class);
+            startActivityForResult(intent,0);
+            return true;
+        }
+        return false;
+    }
+
+    protected void onActivityResult(int requestCode,int resultCode,Intent intent)
+    {
+
+        if(requestCode==0)
+        {
+
+            if (resultCode==RESULT_OK)
+            {
+                Bundle extras=intent.getExtras();
+                boolean cyclemap = extras.getBoolean("com.example.cyclemap");
+                if(cyclemap==true)
+                {
+                    mv.setTileSource(TileSourceFactory.CYCLEMAP);
+                }
+                else
+                {
+                    mv.getTileProvider().setTileSource(TileSourceFactory.MAPNIK);
+                }
+            }
+        }
+    }
+
 }
---
+++ app/src/main/java/com/example/a2ramae45/mapping/MapChooseActivity.java
@@ -0,0 +1,43 @@
+package com.example.a2ramae45.mapping;
+
+import android.app.Activity;
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.widget.Button;
+import android.view.View;
+
+/**
+ * Created by 2ramae45 on 09/02/2017.
+ */
+public class MapChooseActivity extends Activity implements View.OnClickListener {
+
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.mca);
+
+
+        Button regular = (Button)findViewById(R.id.btnRegular);
+        regular.setOnClickListener(this);
+        Button cyclemap = (Button)findViewById(R.id.btnCyclemap);
+        cyclemap.setOnClickListener(this);
+    }
+
+
+    public void onClick(View v)
+    {
+        Intent intent = new Intent();
+        Bundle bundle=new Bundle();
+        boolean cyclemap=false;
+        if (v.getId()==R.id.btnCyclemap)
+        {
+            cyclemap=true;
+        }
+        bundle.putBoolean("com.example.cyclemap",cyclemap);
+        intent.putExtras(bundle);
+        setResult(RESULT_OK,intent);
+        finish();
+    }
+
+}
